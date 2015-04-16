9770686cbd36334b36be95572c7c07a332ba1b8b
Bridget McErlean <bridgetmcerlean@gmail.com>
2015-04-16 20:35:53 +0100

    tableflip!

---
+++ tableflip.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+face()
+{
+    echo -e "\033[u\033[1C\x28\033[1C\xc2\xb0\033[1C\xc2\xb0\x29"
+}
+
+table()
+{
+    face
+    echo -e "\033[u\033[2C\xe2\x95\xb7\033[1C\xe2\x96\xad\033[2C\xe2\x95\xb7"
+    echo -e "\033[u\033[8C \xe2\x94\xb3\xe2\x94\x81\xe2\x94\xb3  "
+    sleep 0.5
+}
+
+tableflip()
+{
+    face
+    echo -e "\033[u\033[2C\xe2\x95\xaf\033[1C\xe2\x96\xa1\033[2C\xe2\x95\xaf"
+    echo -e "\033[u\033[8C\xef\xb8\xb5 \xe2\x94\xbb\xe2\x94\x81\xe2\x94\xbb"
+    sleep 0.5
+}
+
+echo -e "\n\033[1F\033[s"
+while [ 1 = 1 ]
+do
+    table
+    tableflip
+done
